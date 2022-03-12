5a532de668543a47ede9034883d4da909c62d76f
= <=>
2022-03-12 16:20:40 -0500

    work in progress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    base: '/docs/',
+    dest: 'public/docs',
+    title:'Docs',
+    description:'This is description',
+    head: [
+      ['link', { rel: 'icon', href: 'https://picsum.photos/100' }]
+    ],
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Install', link: '/getstarted/' }
+      ],
+      sidebar: [
+        ['/', 'Getting Started'],
+        {
+          title: 'Getting Started',
+          collapsable: false,
+          children: [
+            ['/getstarted/', 'Install it title'],
+            ['/policies/', 'Policy'],
+            ['/todo-roadmap/', 'Roadmap']
+          ]
+        }
+      ]
+    }
+  }
